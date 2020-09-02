eafb55908289438dc777c6c034c9b8b036c77e42
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2020-09-02 15:03:33 -0400

    Add project files.

---
+++ Dummy2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder SB = new StringBuilder("Jumagos al 8Ball?");
+            SB.AppendLine("This ends now.");
+
+            Console.WriteLine(SB.ToString());
+
+        }
+    }
+}
